4b486bdfc5963fe7918e32d52734e0b5a27e96ca
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-07-05 23:44:25 +0200

    Testing with switch system. Refactor it.

---
+++ index.js
@@ -0,0 +1,51 @@
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    app        = express(),
+    Datastore  = require('nedb'),
+    db         = {};
+
+db.crips = new Datastore({
+  filename: 'db/crips',
+  autoload: true
+});
+
+app.use(bodyParser.json());
+
+app.get('/', function( req, res ) {
+  res.send("The API is working.");
+})
+.post('/rpc', function( req, res) {
+  var body    = req.body,
+      respond = function( err, results ) {
+        if ( err ) {
+          res.send( JSON.stringify(err) );
+        }
+        else {
+          res.send( JSON.stringify(results) );
+        }
+      };
+
+  res.set('Content-Type', 'application/json');
+
+  switch ( body.action ) {
+    case "getCrips":
+      db.crips.find({}, respond);
+      break;
+
+    case "addCrip":
+      db.crips.insert({ name: body.name }, respond);
+      break;
+
+    case "positionCrip":
+      db.crips.update({ name: body.name },
+        { $set: { position: body.position }
+      }, function( err, num) {
+        respond(err, { success: num + " records updated" });
+      });
+      break;
+
+    default:
+      respond("No action given");
+  }
+})
+.listen(1337);
